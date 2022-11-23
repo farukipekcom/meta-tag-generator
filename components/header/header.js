caaad91e4c3707a4069c9b44b9fd59548f99cd37
Faruk İpek <farukipekweb@gmail.com>
2022-11-22 19:22:51 -0500

    fix: add dropdown menu

--- components/header/header.js
+++ components/header/header.js
@@ -1,6 +1,11 @@
 import Link from "next/link";
 import styles from "./header.module.css";
+import { useState } from "react";
 const Header = () => {
+  const [menu, setMenu] = useState(false);
+  const deneme = () => {
+    setMenu(!menu);
+  };
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
@@ -23,9 +28,46 @@ const Header = () => {
         <Link href="/open-graph" className={styles.item}>
           Open Graph
         </Link>
-        <Link href="/twitter-card" className={styles.item}>
-          Twitter Card
-        </Link>
+        <div className={`${styles.item}`} onClick={deneme}>
+          <Link href="/twitter-card">Twitter Card</Link>
+          <svg
+            width="14"
+            height="8"
+            viewBox="0 0 14 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className={styles.svg}
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.5421 0L6.98282 5.19016L1.38969 0.0330012L0 1.38829L6.94924 8L14 1.42129L12.5421 0Z"
+              fill="#D1D5DB"
+            />
+          </svg>
+        </div>
+
+        {menu === true ? (
+          <div className={styles.dropdown}>
+            <Link href="/twitter-app" className={styles.item}>
+              App
+            </Link>
+            <Link href="/twitter-player" className={styles.item}>
+              Player
+            </Link>
+            <Link href="/twitter-summary" className={styles.item}>
+              Summary
+            </Link>
+            <Link
+              href="/twitter-summary-with-large-image"
+              className={styles.item}
+            >
+              Summary with Large Image
+            </Link>
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
