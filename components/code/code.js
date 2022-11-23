bf379715a6e59a3ad71c526667f6b7e55d1fd9d2
Faruk İpek <farukipekweb@gmail.com>
2022-11-22 21:37:40 -0500

    feat: add code copy

--- components/code/code.js
+++ components/code/code.js
@@ -1,11 +1,28 @@
 import Highlight from "react-highlight";
 import styles from "./code.module.css";
+import { useState } from "react";
 const Code = ({ data }) => {
+  const [message, setMessage] = useState();
+  const [status, setStatus] = useState();
   return (
     <div className={styles.main}>
       <div className={styles.title}>Code</div>
-      <div className={styles.code}>
+      <div className={`${styles.code} ${status && styles.copied}`}>
         <Highlight className="html">{data}</Highlight>
+        <button
+          className={styles.copy}
+          onClick={() => {
+            navigator.clipboard.writeText(data);
+            setMessage(1);
+            setStatus(1);
+            setTimeout(() => {
+              setMessage("");
+            }, 2000);
+          }}
+        >
+          Copy
+        </button>
+        {message && <div className={styles.message}>Copied</div>}
       </div>
     </div>
   );
